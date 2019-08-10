bbd56d2c6289ded37f208f71822619e0bd3a0295
wchen329 <wchen329@wisc.edu>
2019-08-10 17:58:49 -0500

    Set Qt application icon

--- simUI/main.cpp
+++ simUI/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QIcon mainIco = QIcon(":/icons/art/priscas_icon_24.ico");
+    QApplication::setWindowIcon(mainIco);
     simUI w;
     w.show();
 
