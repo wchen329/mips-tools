c27d378dafda50793ad10210972ebee00a35e96f
wchen329 <wchen329@wisc.edu>
2019-08-21 13:58:31 -0500

    Change versioning to 0.3.9.552

--- include/branding.h
+++ include/branding.h
@@ -23,9 +23,9 @@
 
 // These predefines are specifically used for Windows Resource Compiler
 #define PRISCAS_BRANDING_AUTHOR "Winor Chen"
-#define PRISCAS_BRANDING_FULL_VERSION_STRING "0.3.2019.8"
+#define PRISCAS_BRANDING_FULL_VERSION_STRING "0.3.9.552"
 #define PRISCAS_BRANDING_COPYRIGHT "Copyright (c) 2019 Winor Chen"
-#define PRISCAS_BRANDING_FULL_VERSION 0,3,2019,8
+#define PRISCAS_BRANDING_FULL_VERSION 0,3,9,552
 
 /* Header that includes information about project name, description, and versioning, etc., etc.
  * wchen329
